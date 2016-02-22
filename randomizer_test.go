0274f8de6bd5ae3fe8c642e4f47bc6c2873e5fd3
Tim Heckman <tim@pagerduty.com>
2016-02-21 18:48:03 -0800

    add some utilities for generating random data
    This adds some tests and benchmarks for a package to generate random data from `crypto/rand`.
    
    On a `MacBook Pro (Retina, 15-inch, Late 2013)`:
    
    ```
    === RUN   Test
    PASS: randomizer_test.go:66: TestSuite.BenchmarkGenerateRandomBase64	 1000000	      1065 ns/op	  15.01 MB/s
    PASS: randomizer_test.go:74: TestSuite.BenchmarkGenerateRandomBase64URLEncoding	 1000000	      1054 ns/op	  15.18 MB/s
    PASS: randomizer_test.go:27: TestSuite.BenchmarkGenerateRandomBytesBy1	10000000	       244 ns/op	   4.10 MB/s
    PASS: randomizer_test.go:41: TestSuite.BenchmarkGenerateRandomBytesBy128	  200000	      8078 ns/op	  15.84 MB/s
    PASS: randomizer_test.go:34: TestSuite.BenchmarkGenerateRandomBytesBy64	  500000	      4135 ns/op	  15.48 MB/s
    PASS: randomizer_test.go:82: TestSuite.BenchmarkGenerateRandomUint16	 5000000	       325 ns/op	   6.14 MB/s
    PASS: randomizer_test.go:89: TestSuite.BenchmarkGenerateRandomUint32	 5000000	       476 ns/op	   8.39 MB/s
    PASS: randomizer_test.go:96: TestSuite.BenchmarkGenerateRandomUint64	 5000000	       706 ns/op	  11.32 MB/s
    OK: 8 passed
    --- PASS: Test (17.79s)
    PASS
    ok  	github.com/theckman/go-randomizer	17.794s
    ```

---
+++ randomizer_test.go
@@ -0,0 +1,101 @@
+package randomizer_test
+
+import (
+	"testing"
+
+	"github.com/theckman/go-randomizer"
+
+	. "gopkg.in/check.v1"
+)
+
+type TestSuite struct{}
+
+var _ = Suite(&TestSuite{})
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (*TestSuite) TestGenerateRandomBytes(c *C) {
+	// I have no fucking idea how to test this shit...
+	var b []byte
+	var err error
+
+	b, err = randomizer.GenerateRandomBytes(10)
+	c.Assert(err, IsNil)
+	c.Check(len(b), Equals, 10) // ¯\_(ツ)_/¯
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomBytesBy1(c *C) {
+	for i := 0; i < c.N; i++ {
+		randomizer.GenerateRandomBytes(1)
+		c.SetBytes(1)
+	}
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomBytesBy64(c *C) {
+	for i := 0; i < c.N; i++ {
+		randomizer.GenerateRandomBytes(64)
+		c.SetBytes(64)
+	}
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomBytesBy128(c *C) {
+	for i := 0; i < c.N; i++ {
+		randomizer.GenerateRandomBytes(128)
+		c.SetBytes(128)
+	}
+}
+
+func (*TestSuite) TestGenerateRandomBase64(c *C) {
+	var s string
+	var err error
+
+	s, err = randomizer.GenerateRandomBase64(32)
+	c.Assert(err, IsNil)
+	c.Check(len(s), Equals, 32)
+}
+
+func (*TestSuite) TestGenerateRandomBase64URLEncoding(c *C) {
+	var s string
+	var err error
+
+	s, err = randomizer.GenerateRandomBase64URLEncoding(32)
+	c.Assert(err, IsNil)
+	c.Check(len(s), Equals, 32)
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomBase64(c *C) {
+	var s string
+	for i := 0; i < c.N; i++ {
+		s, _ = randomizer.GenerateRandomBase64(16)
+		c.SetBytes(int64(len(s)))
+	}
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomBase64URLEncoding(c *C) {
+	var s string
+	for i := 0; i < c.N; i++ {
+		s, _ = randomizer.GenerateRandomBase64URLEncoding(16)
+		c.SetBytes(int64(len(s)))
+	}
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomUint16(c *C) {
+	for i := 0; i < c.N; i++ {
+		randomizer.GenerateRandomUint16()
+		c.SetBytes(2)
+	}
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomUint32(c *C) {
+	for i := 0; i < c.N; i++ {
+		randomizer.GenerateRandomUint32()
+		c.SetBytes(4)
+	}
+}
+
+func (t *TestSuite) BenchmarkGenerateRandomUint64(c *C) {
+	for i := 0; i < c.N; i++ {
+		randomizer.GenerateRandomUint64()
+		c.SetBytes(8)
+	}
+}
