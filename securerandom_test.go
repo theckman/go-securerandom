fd04d8f280035791ff80716e148734079475fa5c
Tim Heckman <tim@pagerduty.com>
2016-02-21 21:07:57 -0800

    rename package to securerandom
    The name `randomizer` was OK, but `securerandom` is a bit more fitting. So let's rename it, as well as some of the functions to be easier to work with.

---
+++ securerandom_test.go
@@ -0,0 +1,189 @@
+// Copyright 2016 Tim Heckman. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause
+// license that can be found in the LICENSE file.
+
+package securerandom_test
+
+import (
+	"testing"
+
+	"github.com/theckman/go-securerandom"
+
+	. "gopkg.in/check.v1"
+)
+
+type TestSuite struct{}
+
+var _ = Suite(&TestSuite{})
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (*TestSuite) TestBytes(c *C) {
+	// I have no fucking idea how to test this shit...
+	var b []byte
+	var err error
+
+	b, err = securerandom.Bytes(10)
+	c.Assert(err, IsNil)
+	c.Check(len(b), Equals, 10) // ¯\_(ツ)_/¯
+}
+
+func (t *TestSuite) BenchmarkBytesBy1(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Bytes(1)
+		c.SetBytes(1)
+	}
+}
+
+func (t *TestSuite) BenchmarkBytesBy16(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Bytes(16)
+		c.SetBytes(16)
+	}
+}
+
+func (t *TestSuite) BenchmarkBytesBy32(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Bytes(32)
+		c.SetBytes(32)
+	}
+}
+
+func (t *TestSuite) BenchmarkBytesBy64(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Bytes(64)
+		c.SetBytes(64)
+	}
+}
+
+func (*TestSuite) TestBase64(c *C) {
+	var s string
+	var err error
+
+	s, err = securerandom.Base64(32)
+	c.Assert(err, IsNil)
+	c.Check(len(s), Equals, 32)
+}
+
+func (t *TestSuite) BenchmarkBase64(c *C) {
+	var s string
+	for i := 0; i < c.N; i++ {
+		s, _ = securerandom.Base64(16)
+		c.SetBytes(int64(len(s)))
+	}
+}
+
+func (*TestSuite) TestURLBase64(c *C) {
+	var s string
+	var err error
+
+	s, err = securerandom.URLBase64(32)
+	c.Assert(err, IsNil)
+	c.Check(len(s), Equals, 32)
+}
+
+func (t *TestSuite) BenchmarkURLBase64(c *C) {
+	var s string
+	for i := 0; i < c.N; i++ {
+		s, _ = securerandom.URLBase64(16)
+		c.SetBytes(int64(len(s)))
+	}
+}
+
+func noop(interface{}) {}
+
+func (t *TestSuite) TestUint16(c *C) {
+	var u16 uint16
+	var err error
+
+	u16, err = securerandom.Uint16()
+	c.Assert(err, IsNil)
+	noop(u16)
+}
+
+func (t *TestSuite) BenchmarkUint16(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Uint16()
+		c.SetBytes(2)
+	}
+}
+
+func (t *TestSuite) TestUint32(c *C) {
+	var u32 uint32
+	var err error
+
+	u32, err = securerandom.Uint32()
+	c.Assert(err, IsNil)
+	noop(u32)
+}
+
+func (t *TestSuite) BenchmarUint32(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Uint32()
+		c.SetBytes(4)
+	}
+}
+
+func (t *TestSuite) TestUint64(c *C) {
+	var u64 uint64
+	var err error
+
+	u64, err = securerandom.Uint64()
+	c.Assert(err, IsNil)
+	noop(u64)
+}
+
+func (t *TestSuite) BenchmarkUint64(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Uint64()
+		c.SetBytes(8)
+	}
+}
+
+func (t *TestSuite) TestInt16(c *C) {
+	var i16 int16
+	var err error
+
+	i16, err = securerandom.Int16()
+	c.Assert(err, IsNil)
+	noop(i16)
+}
+
+func (t *TestSuite) BenchmarkInt16(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Int16()
+		c.SetBytes(2)
+	}
+}
+
+func (t *TestSuite) TestInt32(c *C) {
+	var i32 int32
+	var err error
+
+	i32, err = securerandom.Int32()
+	c.Assert(err, IsNil)
+	noop(i32)
+}
+
+func (t *TestSuite) BenchmarkInt32(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Int32()
+		c.SetBytes(4)
+	}
+}
+
+func (t *TestSuite) TestInt64(c *C) {
+	var i64 int64
+	var err error
+
+	i64, err = securerandom.Int64()
+	c.Assert(err, IsNil)
+	noop(i64)
+}
+
+func (t *TestSuite) BenchmarkInt64(c *C) {
+	for i := 0; i < c.N; i++ {
+		securerandom.Uint64()
+		c.SetBytes(8)
+	}
+}
